5d4311da9bcdd37f804abb10a3b918cfae10be7a
Watthachai <wattchaichai@gmail.com>
2024-09-24 22:11:11 +0700

    Added simple-api

---
+++ app/app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Index!"
+
+@app.route('/getcode', methods=['GET'])
+def getcode():
+    return "Hello, This is my code 5555"
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello(name):
+    return "Hello, " + str(name)
+
+
+@app.route('/plus/<num1>/<num2>', methods=['GET'])
+def calculate(num1, num2):
+    try:
+        num1 = eval(num1)
+        num2 = int(num2)
+
+        results = {
+                'plus' : num1 + num2,
+                'minus' : num1 - num2,
+                'multiply': num1 * num2,
+                'divide' : num1/num2
+            }
+    except:
+        results = { 'error_msg' : 'inputs must be numbers' }
+
+    return jsonify(results)
+
+
+if __name__ == '__main__':
+    app.run()
